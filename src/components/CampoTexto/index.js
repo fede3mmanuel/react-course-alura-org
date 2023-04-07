a23d8c8f9cadf0395b04412269c50668c1b0ea38
Emmanuel Sanchez <fede@fede.website>
2023-04-07 14:32:02 -0600

    Componente campo texto

---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,9 @@
+import "./CampoTexto.css"
+
+const CampoTexto = () => {
+    return <div className="campo-texto">
+        <label>Nombre</label>
+        <input placeholder="Ingresar Nombre"/>
+    </div>
+}
+export default CampoTexto
--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,9 +1,13 @@
 import './Formulario.css'
+import CampoTexto from '../CampoTexto'
 
 const Formulario = () => {
     return <section className='formulario'>
         <form>
             <h2>Rellena el formulario para crear el colaborador.</h2>
+            <CampoTexto />
+            <CampoTexto />
+            <CampoTexto />
         </form>
     </section>
 }
