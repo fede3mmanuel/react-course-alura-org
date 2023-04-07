20ac0831e4f046c315d8e68a75e64a86ae76bacf
Emmanuel Sanchez <fede@fede.website>
2023-04-07 11:59:52 -0600

    Usando componente header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
