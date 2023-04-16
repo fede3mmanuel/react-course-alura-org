fd83f19f9dfe58d52c57ad375708af79adb7dff8
Emmanuel Sanchez <fede@fede.website>
2023-04-16 13:13:40 -0600

    Estado con React

--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./MiOrg.css"
 
 const MiOrg = () => {
+
+    const [nombre, actualizarNombre] = useState("Harland")
+    const [mostrar, actualizarMostrar] = useState(true)
+
+    const manejarClick = () => {
+        console.log("Mostrar elemento" + mostrar);
+        actualizarMostrar(!mostrar)
+    }
     return <section className="orgSection">
-        <h3 className="title">Mi organización</h3>
-        <img src="/img/add.png" alt="add" />
+        <h3 className="title">Mi organización {nombre}</h3>
+        <img src="/img/add.png" alt="add" onClick={manejarClick}/>
     </section>
 }
 
