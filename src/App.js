04c92acced7f2dd33ba6dc5e4702b26c94c7abf0
Emmanuel Sanchez <fede@fede.website>
2023-04-18 18:59:32 -0600

    Creando equipos

--- src/App.js
+++ src/App.js
@@ -12,6 +12,53 @@ function App() {
   const cambiarMostrar = () => {
     actualizarMostrar(!mostrarFormulario)
   }
+
+  const equipos = [
+
+    {
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    }
+    ,
+    {
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    }
+    ,
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    }
+    ,
+    {
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    }
+    ,
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#BD6EBF",
+      colorSecundario: "#FAE9F5"
+    }
+    ,
+    {
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    }
+    ,
+    {
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+    
+]
+
   return (
     <div>
       <Header />
@@ -20,7 +67,11 @@ function App() {
       { mostrarFormulario && <Formulario /> }
       
       <MiOrg cambiarMostrar={cambiarMostrar}/>
-      <Equipo />
+      
+      {
+        equipos.map( (equipo) =>  <Equipo datos={equipo} key={equipo.titulo} /> )
+      }
+
     </div>
   );
 }
